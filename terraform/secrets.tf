a6b8ca801284d73b2db99c3e6444c957fdb99d6d
Murillo Lima <mlima@carnegiehighered.com>
2026-04-16 14:52:58 -0300

    cookie tf

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -12,9 +12,22 @@ resource "google_secret_manager_secret" "jwt_private_key" {
   }
 }
 
+resource "google_secret_manager_secret" "cookie_secret" {
+  secret_id = "roi-reports-cookie-secret-${var.environment}"
+  replication {
+    auto {}
+  }
+}
+
 # Grant the Cloud Run SA read access to secrets it needs at runtime
 resource "google_secret_manager_secret_iam_member" "jwt_public_key_access" {
   secret_id = google_secret_manager_secret.jwt_public_key.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.cloud_run_sa.email}"
+}
+
+resource "google_secret_manager_secret_iam_member" "cookie_secret_access" {
+  secret_id = google_secret_manager_secret.cookie_secret.secret_id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.cloud_run_sa.email}"
 }
